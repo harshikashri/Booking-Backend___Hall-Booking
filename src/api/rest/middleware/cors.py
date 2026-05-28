4d633a9fbee334250aa444581e90c583d2efaf31
harshikashri <harshikashribalaraja@gmail.com>
2026-05-28 14:29:28 +0530

    updated

--- src/api/rest/middleware/cors.py
+++ src/api/rest/middleware/cors.py
@@ -9,7 +9,7 @@ FRONTEND_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    " https://frontend-159108267686.asia-south1.run.app",
+    "https://frontend-159108267686.asia-south1.run.app",
 ]
 
 
